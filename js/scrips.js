a79adfbf39f48418da3de5fcae50aa9b553786ff
wahyu1709 <44396432+wahyu1709@users.noreply.github.com>
2021-01-21 16:48:33 +0700

    Update scrips.js

--- js/scrips.js
+++ js/scrips.js
@@ -17,3 +17,19 @@
     }, false);
 })();
 
+
+function validasi(){
+    var username = document.forms["myForm"]["username"].value;
+    var password = document.forms["myForm"]["password"].value;
+if( username == "admin" && password == "admin"){
+    return true;
+}
+else if(username =="" || password ==""){
+    alert("Masukan username dan password Anda");
+    return false;
+}
+else{
+    alert("Username atau password yang Anda masukan SALAH");
+    return false;
+}
+}
